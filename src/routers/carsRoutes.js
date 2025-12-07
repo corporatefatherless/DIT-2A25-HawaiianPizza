fffb8b580f085e2b3f13c35fc7ac449b3dfd7862
NicholasLeow123 <NICHOLASL.24@ichat.sp.edu.sg>
2025-12-07 20:55:58 +0800

    updated car routes

--- src/routers/carsRoutes.js
+++ src/routers/carsRoutes.js
@@ -5,10 +5,7 @@ const carsControllers = require('../controllers/carsControllers');
 // GET all cars
 router.get('/', carsControllers.getAllCars);
 
-// GET car by carsid
+// GET one car by carsid
 router.get('/:carsid', carsControllers.getCar);
-
-// POST create new car
-router.post('/', carsControllers.createCar);
 
 module.exports = router;
